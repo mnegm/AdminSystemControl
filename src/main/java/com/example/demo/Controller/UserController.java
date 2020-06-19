1bea21ed2a64d0fe05c3960d330f170978f69722
mnegm <mnegm262@yahoo.com>
2020-06-19 13:32:51 +0200

    update and delete post added write comments on friends posts to come

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -102,9 +104,33 @@ public class UserController {
 		return "redirect:/user/{id}";
 	}
 	
+	@GetMapping("/posts/{postId}")
+	public String updatePost(@PathVariable int id , @PathVariable int postId, Model model) {
+		Post post = postRepo.getOne(postId);
+		model.addAttribute("post", post);
+		model.addAttribute("user", userRepo.getOne(id));
+		return "updatePost";
+	}
+	
+	@PostMapping("/posts/{postId}")
+	public String deletePost(@PathVariable int postId, @PathVariable int id) {
+		postRepo.delete(postRepo.getOne(postId));
+		return "redirect:/user/{id}";
+	}
 	
 	
-	
+	@PostMapping("/posts/{postId}/update")
+	public String postUpdate(@RequestParam("post") String txt , @PathVariable int id, @PathVariable int postId) {
+		Post post = new Post();
+		post.setPost(txt);
+		post.setId(postId);
+		post.setTime(LocalDateTime.now());
+		post.setUser(userRepo.getOne(id));
+		postRepo.save(post);
+		
+		
+		return "redirect:/user/{id}";
+	}
 	
 	
 	
